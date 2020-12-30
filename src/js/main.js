ea7b447c947562289c03da18a438036ee6f83e4e
Maros Zimnikoval <maros.zimnikoval@universalplant.com>
2020-12-30 14:31:45 +0100

    New items

--- src/js/main.js
+++ src/js/main.js
@@ -61,10 +61,8 @@ $( function() {
 		}
 	});
 
-	$("#menu-trigger").click(function() {  //use a class, since your ID gets mangled
-		$('.left-sidebar').addClass("active");      //add the class to the clicked element
+	$(".hamburger").click(function() {  //use a class, since your ID gets mangled
+		$('.left-sidebar').toggleClass("active");    
+		$(this).toggleClass('is-active');  //add the class to the clicked element
 	});
-	$("#menu-close").click(function() {  //use a class, since your ID gets mangled
-		$('.left-sidebar').removeClass("active");      //add the class to the clicked element
-	});	
 });
